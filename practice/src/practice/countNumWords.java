c3b05ec5cd50fb535f97fb87d29e895554fa16eb
mamin11 <abdimamin@gmail.com>
2020-12-02 21:19:00 +0000

    Q: count num words in string

---
+++ practice/src/practice/countNumWords.java
@@ -0,0 +1,24 @@
+package practice;
+
+public class countNumWords {
+
+	/* QUESTION: 
+	 * Write a Java Program to count the number of words in a string 
+	 * 
+	 * */
+	
+	public static void main(String[] args) {
+		String str = "This is a long sentence. Counting the number of words in a string using a java program";
+		
+		//method 1
+		int n = str.split(" ").length;
+		
+		//method 2
+		//String [] splitStr= str.split(" ");
+		//int count = splitStr.length;
+		
+		System.out.println(n);
+
+	}
+
+}
